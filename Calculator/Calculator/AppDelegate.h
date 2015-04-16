ce9a2ea8db36cebebfa560f9f6a7e2716d493495
金海天 <haitian299@gmail.com>
2015-04-16 10:45:28 +0800

    init

---
+++ Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by JIN on 15/4/16.
+//  Copyright (c) 2015年 JIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
